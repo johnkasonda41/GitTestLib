14dbbd8d41028014114d5e132449d9816363ae90
MalamaKangwa <MalamaKangwa>
2019-12-23 15:55:01 +0200

    Task: Added Test Code

--- GitTestLib/Class1.cs
+++ GitTestLib/Class1.cs
@@ -4,9 +4,17 @@ namespace GitTestLib
 {
     public class Class1
     {
-        public void Print()
+        public void Print(String input)
         {
-
+            Utility.Print(input);
         }
     }
+
+    public static class Utility {
+        public static void Print(string input) 
+        {
+            Console.WriteLine(input);
+        }
+    }
+
 }
