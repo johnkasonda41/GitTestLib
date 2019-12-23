d854edacb6a251260f1e5cc3441a3b812cb74cf6
johnkasonda <john.kasonda@gmail.com>
2019-12-23 15:15:19 +0200

    Add project files.

---
+++ GitTestLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitTestLib
+{
+    public class Class1
+    {
+        public void Print()
+        {
+
+        }
+    }
+}
---
+++ GitTestLibTests/Class1Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitTestLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitTestLib.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
